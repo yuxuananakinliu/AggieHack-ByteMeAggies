7adc7d9bc01b34a89b2895399126a04e0b6cf5b3
AnakinLiu <yuxuananakinliu@gmail.com>
2025-04-14 21:44:50 -0700

    Updated query with filter

--- Code/pysupabase.py
+++ Code/pysupabase.py
@@ -8,16 +8,46 @@ def create_conn(apifile:str):
     sb_client = create_client(supaapi['URL'], supaapi['APIKEY'])
     return sb_client
 
-def rolling_query(api_file:str, table_name:str, target_col:str='*', query_limit:int=1000)->pd.DataFrame:
+def rolling_query(
+        api_file:str, 
+        table_name:str, 
+        target_col:str='*', 
+        query_limit:int=1000,
+        where:dict[str, tuple]=None
+    )->pd.DataFrame:
     '''Query data from Supabase in batches, return all columns by default'''
     all_rows = []
     offset = 0
     sb_client = create_conn(api_file)
 
     while True:
-        response = sb_client.table(table_name).select(
-            target_col
-        ).range(offset, offset+query_limit-1).execute()
+        query = sb_client.table(table_name).select(target_col)
+
+        # Filtering conditions
+        if where:
+            for col, (val, op) in where.items():
+                if op == '=':
+                    query = query.eq(col, val)
+                elif op == '!=':
+                    query = query.neq(col, val)
+                elif op == '>':
+                    query = query.gt(col, val)
+                elif op == '>=':
+                    query = query.gte(col, val)
+                elif op == '<':
+                    query = query.lt(col, val)
+                elif op == '<=':
+                    query = query.lte(col, val)
+                elif op == 'like':
+                    query = query.like(col, val)
+                elif op == 'ilike':
+                    query = query.ilike(col, val)
+                elif op == 'is_':
+                    query = query.is_(col, val)
+                else:
+                    raise ValueError(f"Unsupported operator: {op}")
+
+        response = query.range(offset, offset+query_limit-1).execute()
         if not response.data:
             break
         all_rows.extend(response.data)
